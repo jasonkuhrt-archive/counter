bacd530e803ced920143e3366e38d20a38250295
Jason Kuhrt <jasonkuhrt@me.com>
2014-02-27 09:46:00 -0500

    Move augment to own lib file

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 'use strict';
+var augment = require('./lib/augment');
 
-function augment(a, b){
-  for (var key in b) {
-    if (!b.hasOwnProperty(key)) continue;
-    a[key] = b[key];
-  }
-  return a;
-}
 
 
 
