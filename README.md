d52ea970434605114e7fb240856c5b7c88a7d8ee
Jason Kuhrt <jasonkuhrt@me.com>
2014-02-27 09:44:19 -0500

    Add counter code

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-counter
-=======
+# counter
 
 Functional utility for bookkeeping a changing number
+
+
+# Installation
+
+    npm install counter
+
+
+# API
+
+#### Counter
+#### value
+#### inc
+#### dec
+#### incBy
+#### decBy
+#### reset
---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+function augment(a, b){
+  for (var key in b) {
+    if (!b.hasOwnProperty(key)) continue;
+    a[key] = b[key];
+  }
+  return a;
+}
+
+
+
+function Counter(from){
+  var count = {};
+
+  count.conf = {
+    from: from
+  };
+
+  count.state = {
+    resets: [],
+    value: from
+  };
+
+  for (var key in api) {
+    if (!api.hasOwnProperty(key)) continue;
+    count[key] = api[key].bind(null, count);
+  }
+
+  return count;
+}
+
+
+
+var api = {};
+
+api.reset = function reset(counter){
+  counter.state.resets.push(counter.value);
+  counter.state.value = counter.conf.from;
+  return counter;
+};
+
+api.clear = function clear(counter){
+  api.reset(counter);
+  counter.state.resets.length = 0;
+  return counter;
+};
+
+api.value = function value(counter){
+  return counter.state.value;
+};
+
+api.incBy = function incBy(counter, by){
+  counter.state.value += by;
+  return counter;
+};
+
+api.inc = function inc(counter){
+  return api.incBy(counter, 1);
+};
+
+api.decBy = function decBy(counter, by){
+  return api.incBy(counter, by * -1);
+};
+
+api.dec = function dec(counter){
+  return api.decBy(counter, 1);
+};
+
+
+
+module.exports = Counter;
+module.exports.Counter = Counter;
+augment(module.exports, api);
